34c105d21ef50b0a0cfa366d6e61969fb89e3752
Rindra Hosea <mahefaniainarindra@gmail.com>
2024-07-10 12:48:32 +0300

    add localhost to host

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,8 +1,12 @@
 from .settings import *
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["3.129.128.0", "ec2-3-129-128-0.us-east-2.compute.amazonaws.com"]
+ALLOWED_HOSTS = [
+    "3.129.128.0",
+    "ec2-3-129-128-0.us-east-2.compute.amazonaws.com",
+    "localhost",
+]
 
 DATABASES = {
     "default": {
@@ -14,6 +18,7 @@ DATABASES = {
 CSRF_TRUSTED_ORIGINS = [
     "3.129.128.0",
     "ec2-3-129-128-0.us-east-2.compute.amazonaws.com",
+    "localhost",
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
