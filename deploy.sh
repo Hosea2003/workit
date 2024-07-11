d9b50bdb3c3a18a331c73fb96b3658b50f19ab30
Rindra Hosea <mahefaniainarindra@gmail.com>
2024-07-11 16:13:01 +0300

    temporary remove collectstatic

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ echo "DEPLOYING APP"
 sudo docker compose down
 sudo docker compose up --build -d
 # collectstatic
-sudo docker exec -it workit python manage.py collectstatic --no-input
+# sudo docker exec -it workit python manage.py collectstatic --no-input
