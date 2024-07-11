b13aed998619ff2fecf95d90fac60d9f51793ab8
Rindra Hosea <mahefaniainarindra@gmail.com>
2024-07-11 16:49:05 +0300

    fix static files

--- core/settings/local.py
+++ core/settings/local.py
@@ -1,4 +1,4 @@
 from .settings import *
 
 ALLOWED_HOSTS = ["*"]
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
