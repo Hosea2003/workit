4f402424805c0c2e2c123bf88da89b0a829b3579
Rindra Hosea <mahefaniainarindra@gmail.com>
2024-07-12 17:12:15 +0300

    add new third party

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 echo "DEPLOYING APP"
-# sudo docker exec -it workit python manage.py collectstatic --no-input
+sudo docker exec -it workit python manage.py collectstatic --no-input
 sudo docker compose down
 sudo docker compose up --build -d
